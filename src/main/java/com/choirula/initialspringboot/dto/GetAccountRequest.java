c1b33e0013ca096807c7e036aab777a557c6c7f2
Choirul Andriansyah <choirulandriansyah@choiruls-MacBook-Air.local>
2021-06-24 13:14:31 +0700

    Add get account request

---
+++ src/main/java/com/choirula/initialspringboot/dto/GetAccountRequest.java
@@ -0,0 +1,7 @@
+package com.choirula.initialspringboot.dto;
+
+import java.io.Serializable;
+
+public class GetAccountRequest implements Serializable {
+    private static final long serialVersionUID = -7921977841438263301L;
+}
